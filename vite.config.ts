76e219710f9d3dca0e8abe0cac4a1614cfbc50a2
alumno405466-beep <alumno.405466@ies-azarquiel.es>
2026-04-28 10:43:48 +0200

    Añadido deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/',
+  plugins: [
+    react(),
+    visualizer({ open: true, filename: 'dist/stats.html' }) // abre el reporte
+  ],
+  base: '/webkids/',
 })
